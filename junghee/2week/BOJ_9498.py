caeb4a778f20efe59303db2d6f710565bb62fe6c
DESKTOP-1I0NE7O\82107 <1009totoro@naver.com>
2024-08-29 23:30:54 +0900

    feat: 시험 성적 BOJ_9498.py

---
+++ junghee/2week/BOJ_9498.py
@@ -0,0 +1,16 @@
+score = int(input())  # 사용자로부터 점수를 입력받음
+
+if 90 <= score <= 100:
+    grade = 'A'
+elif 80 <= score < 90:
+    grade = 'B'
+elif 70 <= score < 80:
+    grade = 'C'
+elif 60 <= score < 70:
+    grade = 'D'
+else:
+    grade = 'F'
+
+print(grade)  # 결과 출력
+
+
